76fc5dd297aaacc711d835cd569703b2b951577d
Dmitry <scannorone@gmail.com>
2017-01-31 12:50:56 +0700

    Task02

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,10 @@
 module Main where
 
 import Task01 (task01start)
+import Task02 (task02start)
 import Task03 (task03start)
 import Task04 (task04start)
 import Task11 (task11start)
 import Task13 (task13start)
 
-main = putStr(show(task04start))
+main = putStr(show(task02start))
---
+++ src/Task02.hs
@@ -0,0 +1,10 @@
+module Task02 where
+
+getNumber 0 = []
+getNumber n = (n : getNumber (n - 1))
+
+getDividerList n = [x | x <- (getNumber (n - 1)), (mod n x) == 0]
+
+getPerfectNumber n = [x | x <- reverse . getNumber $ n, x == (sum . getDividerList $ x)]
+
+task02start = getPerfectNumber 10000
