5e2cc1efa7011c449683b5d25fb44bd9186d6c03
Dmitry <scannorone@gmail.com>
2017-01-31 14:17:22 +0700

    Task06

---
+++ src/Task06.hs
@@ -0,0 +1,13 @@
+module Task06 where
+
+data Tree v = Leaf v | Mul (Tree v) (Tree v) | Div (Tree v) (Tree v) | Plus (Tree v) (Tree v) | Minus (Tree v) (Tree v)
+
+calc (Mul left right) = calc left * calc right
+calc (Div left right) = calc left / calc right
+calc (Plus left right) = calc left + calc right
+calc (Minus left right) = calc left - calc right
+calc (Leaf value) = value
+
+task06start = do
+    let tree = Div (Plus (Plus (Plus (Mul (Plus (Leaf 5) (Leaf 3)) (Leaf 4)) (Div (Mul (Leaf 8) (Leaf 20)) (Leaf 10))) (Mul (Minus (Leaf 4) (Leaf 3)) (Leaf 8))) (Div (Leaf 8) (Mul (Leaf 9) (Leaf 4)))) (Leaf 10)
+    calc tree
